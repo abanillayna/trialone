bf2742bd92a6859bb8588e90871dcb897bc1a91b
abanillayna <148599813+abanillayna@users.noreply.github.com>
2023-10-25 19:28:34 +0800

    Add files via upload

---
+++ appointment.js
@@ -0,0 +1,42 @@
+function nine() { 
+    document.querySelector(".nine").onclick = function() { 
+        $("#abc :input").prop("disabled", false);
+    } 
+} 
+
+function ten() { 
+    document.querySelector(".ten").onclick = function() { 
+        $("#abc :input").prop("disabled", false);
+    } 
+} 
+
+
+function eleven() { 
+    document.querySelector(".eleven").onclick = function() { 
+        $("#abc :input").prop("disabled", false);
+    } 
+}
+
+function one() { 
+    document.querySelector(".one").onclick = function() { 
+        $("#abc :input").prop("disabled", false);
+    } 
+} 
+                          
+function two() { 
+    document.querySelector(".two").onclick = function() { 
+        $("#abc :input").prop("disabled", false);
+    } 
+} 
+
+function three() { 
+    document.querySelector(".three").onclick = function() { 
+        $("#abc :input").prop("disabled", false);
+    } 
+} 
+
+/*function displaytime() {
+    var text = document.getElementById("time");
+    text.style.display = "block";
+  }*/
+
